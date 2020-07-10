24fec23504c6af94449c0fd2bb1e95efa25331a0
chingizdiarov <chingizdiarov@gmail.com>
2020-07-10 21:31:46 +0300

    Add login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../_services/user.service";
+import { FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import { MessagesService } from "../_services/messages.service";
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+    form:FormGroup;
+    showErrorMessage: boolean;
+
+    constructor(private fb:FormBuilder,
+                 private authService: UserService,
+                 private router: Router,
+                 private messagesService: MessagesService) {
+
+        this.form = this.fb.group({
+            username: ['',Validators.required],
+            password: ['',Validators.required]
+        });
+    }
+
+    login() {
+        const val = this.form.value;
+        this.showErrorMessage = false;
+
+        if (val.username && val.password) {
+            this.authService.login(val.username, val.password)
+                .subscribe(
+                    () => {
+                        this.router.navigateByUrl('/');
+                    },
+                  (error) => {
+                      this.showErrorMessage = true;
+                  }
+                );
+        }
+
+    }
+}
+
+
+
---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../_services/user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent {
+
+  constructor(private authService: UserService,
+              private router: Router) {
+  }
+
+  logout() {
+    this.authService.logout();
+    console.log("User is logged out:");
+    this.router.navigateByUrl('/login');
+
+
+  }
+}
