9b8e841b428624a39d19c0006b18a39c6016d106
chingizdiarov <chingizdiarov@gmail.com>
2020-08-14 23:37:20 +0300

    Корректировка чекбокса in_stock

--- src/app/flavour-detail/flavour-detail.component.ts
+++ src/app/flavour-detail/flavour-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit} from '@angular/core';
-import { Flavour } from "../flavour";
+import {Flavour, UpdateFlavour} from "../flavour";
 import { NewFlavour } from "../flavour";
 import {ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
 import { FlavourService } from "../_services/flavour.service"
 import { ManufacturerService } from "../_services/manufacturer.service";
 import { Manufacturer } from "../manufacturer";
+import {FlavourTypeService} from "../_services/flavour-type.service";
+import {FlavourType} from "../flavour-type";
 
 @Component({
   selector: 'app-flavour-detail',
@@ -20,30 +22,31 @@ export class FlavourDetailComponent implements OnInit {
     private flavourServuce: FlavourService,
     private location: Location,
     private manufacturerService: ManufacturerService,
+    private flavourTypeService: FlavourTypeService
   ) { }
 
   flavour: Flavour;
   manufacturer: Manufacturer;
   manufacturers: Manufacturer[];
+  flavourTypes: FlavourType[];
   manufacturerName: string;
   manufacturerId: number;
   selectedManufacturer: number;
-  checked: boolean = false;
+  checked: boolean;
 
   ngOnInit(): void {
     this.getFlavour();
     this.getManufacturerList();
+    this.getFlavourTypeList()
   }
 
   getFlavour(): void {
       const id = +this.route.snapshot.paramMap.get('id');
       this.flavourServuce.getFlavour(id)
     .subscribe(flavour => this.flavour = flavour);
-      // this.manufacturerService.getManufacturer(this.flavour.manufacturer).subscribe(manufacturer => this.manufacturer = manufacturer)
   }
 
   save(): void {
-    this.flavour.in_stock = this.checked;
     this.flavourServuce.updateFlavour(this.flavour)
       .subscribe(() => this.goBack());
   }
@@ -52,17 +55,21 @@ export class FlavourDetailComponent implements OnInit {
     this.location.back();
   }
 
-
   getManufacturerList(): void {
       this.manufacturerService.getManufacturers().subscribe(manufacturers => this.manufacturers = manufacturers);
   }
 
-  changeValue(value) {
+  changeValue() {
     if(this.flavour.in_stock === true){
-      this.checked = value;
+      this.checked = false;
     }else{
-      this.checked = !value;
+      this.checked = true;
     }
+    this.flavour.in_stock = this.checked;
+  }
+  getFlavourTypeList(): void {
+    this.flavourTypeService.getFlavourTypes()
+      .subscribe(flavourTypes => this.flavourTypes = flavourTypes);
   }
 }
 
