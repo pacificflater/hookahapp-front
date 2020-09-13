c32eb542870c288101a04c84f9f8ee871c2e0984
chingizdiarov <chingizdiarov@gmail.com>
2020-09-13 17:05:31 +0300

    Добавлена сортировка

--- src/app/manufacturer.ts
+++ src/app/manufacturer.ts
@@ -1,5 +1,6 @@
 export interface Manufacturer {
   id: number;
   name: string;
+  type: {id: number, type: string};
   flavours: [{flavour_name: string, in_stock: boolean}];
 }
--- src/app/manufacturers/manufacturers.component.ts
+++ src/app/manufacturers/manufacturers.component.ts
@@ -1,48 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { Manufacturer } from '../manufacturer';
-import { ManufacturerService } from "../_services/manufacturer.service";
-import { MessagesService } from "../_services/messages.service";
-import { ManufacturerAddComponent} from "../manufacturer-add/manufacturer-add.component";
-import { MatDialog} from "@angular/material/dialog";
-import {MatTableDataSource} from "@angular/material/table";
-
+import { ManufacturerService } from '../_services/manufacturer.service';
+import { MessagesService } from '../_services/messages.service';
+import { ManufacturerAddComponent} from '../manufacturer-add/manufacturer-add.component';
+import { MatDialog} from '@angular/material/dialog';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import {MixesItem} from '../mixes/mixes-datasource';
 
 
 @Component({
   selector: 'app-manufacturers',
   templateUrl: './manufacturers.component.html',
-  styleUrls: ['./manufacturers.component.css']
+  styleUrls: ['./manufacturers.component.scss']
 })
-export class ManufacturersComponent implements OnInit {
+export class ManufacturersComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<MixesItem>;
+  dataSource: any;
+
+  columnsToDisplay = ['name',  'type.type', 'buttons'];
 
   constructor(private manufacturerService: ManufacturerService,
               public messageService: MessagesService,
-              public dialog: MatDialog) {}
+              public dialog: MatDialog
+) {}
 
-  // manufacturers: Manufacturer[];
-  columnsToDisplay = ['name', 'buttons'];
-  public dataSource;
+  manufacturers: Manufacturer[];
 
   ngOnInit(): void {
     this.getManufacturers();
+    this.ngAfterViewInit();
   }
 
   getManufacturers(): void {
     this.manufacturerService.getManufacturers()
-        // .subscribe(manufacturers => this.manufacturers = manufacturers);
       .subscribe(res => {
-        this.dataSource = new MatTableDataSource(res)
-      })
+        this.dataSource = new MatTableDataSource(res);
+      });
   }
 
   deleteManufacturer(manufacturer: Manufacturer): void {
-    this.manufacturerService.deleteManufacturer(manufacturer).subscribe(()=> this.messageService.add(`${manufacturer.name} manufacturer deleted`));
+    this.manufacturerService.deleteManufacturer(manufacturer).subscribe(() => this.messageService.add(`${manufacturer.name} manufacturer deleted`));
+  }
+
+  ngAfterViewInit() {
+    this.table.dataSource = this.dataSource;
+    this.dataSource.sortingDataAccessor = (item, property) => {
+    switch (property) {
+      case 'type.type': return item.type.type;
+      default: return item[property];
+    }
+  };
+    this.dataSource.sort = this.sort;
   }
 
   openDialog() {
     const dialogRef = this.dialog.open(ManufacturerAddComponent);
     dialogRef.afterClosed().subscribe(result => {
-      this.messageService.add(`${result} manufacturer added`)
+      this.messageService.add(`${result} manufacturer added`);
       console.log(`Dialog result: ${result}`);
     });
   }
@@ -51,6 +67,29 @@ export class ManufacturersComponent implements OnInit {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  // filterChanged(event: FilterChangedEvent) {
+  //   const filterModel = event.api.getFilterModel();
+  //   this.filterService.persistFilters(filterModel);
+  //   const params = this.filterService.getQueryParamsFromFilters();
+  //   this.router.navigate([], {
+  //     relativeTo: this.route,
+  //     queryParams: params,
+  //     queryParamsHandling: 'merge'
+  //   });
+  // }
+
+  // clearAllFilters() {
+  //   this.gridApi.setFilterModel(null);
+  // }
+  //
+  // onGridReady(params: { api: GridApi; columnApi: ColumnApi; type: string }) {
+  //   // ...
+  //   const filters = this.filterService.getFiltersFromQueryParams(
+  //     this.route.snapshot.queryParams
+  //   );
+  //   filters && this.gridApi.setFilterModel(filters);
+  // }
 
 }
 
