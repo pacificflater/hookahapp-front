162ebc1015f252df63433e0a56e7e0be3835402b
chingizdiarov <chingizdiarov@gmail.com>
2020-12-16 11:31:07 +0300

    + flavour ID to model

--- src/app/_models/manufacturer.ts
+++ src/app/_models/manufacturer.ts
@@ -2,5 +2,5 @@ export interface Manufacturer {
   id: number;
   name: string;
   type: {id: number, type: string};
-  flavours: [{flavour_name: string, in_stock: boolean}];
+  flavour: [{id: number, flavour_name: string, in_stock: boolean}];
 }
